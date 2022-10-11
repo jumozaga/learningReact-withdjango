e66008a590d37c614fcbd04b945844bc51c1d3f7
jumozaga <jumozaga@yahoo.com>
2022-10-11 20:46:33 -0300

    django views to json

--- backend/zona/views.py
+++ backend/zona/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'app/home.html')
+   # return render(request, 'app/home.html')
+   return JsonResponse({'message': 'Hello, world!'})
