8e762530e2405f6c4e41d0109d9a99cb263b616b
jumozaga <jumozaga@yahoo.com>
2022-10-12 00:44:55 -0300

    Setup to decimal to be decimal

--- backend/brasil/settings.py
+++ backend/brasil/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Faz valores numéricos ser float e não string
+REST_FRAMEWORK = {
+    "COERCE_DECIMAL_TO_STRING": False
+}
