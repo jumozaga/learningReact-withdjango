8ca7a0c3111eb909a185dedc816803accc784fbd
jumozaga <jumozaga@yahoo.com>
2022-10-12 00:35:55 -0300

    adicting validates on serializers

--- backend/zona/serializers.py
+++ backend/zona/serializers.py
@@ -16,7 +16,18 @@ class CadastrarEstadoSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     population = serializers.IntegerField()
     photo = serializers.ImageField()
-
+    
+    def validate_name(self, value):
+        if Estado.objects.filter(name=value).exists():
+            raise serializers.ValidationError("Estado já existe")
+        elif len(value) < 3:
+            raise serializers.ValidationError("Nome do estado muito curto")
+        return value
+    
+    def validate_population(self, value):
+        if value < 1000:
+            raise serializers.ValidationError("População muito pequena")
+        return value
 
 
    
